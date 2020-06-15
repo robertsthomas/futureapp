9ae3278b8829d237a0a2241994543471e4f2f722
Thomas Roberts <robertsthomasd@gmail.com>
2020-06-14 22:40:02 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const config = {
-  apiKey: "AIzaSyB9IpX8sHJRLORzBAWhLU1uwZ4sRQJrEqM",
-  authDomain: "futureapp-cc4b9.firebaseapp.com",
-  databaseURL: "https://futureapp-cc4b9.firebaseio.com",
-  projectId: "futureapp-cc4b9",
-  storageBucket: "futureapp-cc4b9.appspot.com",
-  messagingSenderId: "558658960306",
-  appId: "1:558658960306:web:0d1d7547639607321bc2ab"
+  
 };
 
 firebase.initializeApp(config)
